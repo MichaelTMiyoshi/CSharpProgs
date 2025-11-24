54c29f793286ddcb6bb0ea3a536a19eeebd7c17c
michaeltmiyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2025-11-23 18:28:41 -0800

    11/23/2025 Added function (method) example.

---
+++ FunctionExample/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FunctionExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int a = 4;
+        Console.WriteLine($"a = {a}");
+        int ans = fn1(ref a);
+        Console.Write($"a = {a}\nans = {ans}");
+
+        Console.Write("\n\nPress the any key to continue...");
+        Console.ReadKey();
+    }
+
+    static int fn1(ref int a)
+    {
+        a = a * 3;
+        int b = a - 7;
+        return b;
+    }
+}
