3d201244b605292a4cccb862f43b042a02d7d64e
MichaelTMiyoshi <18077267+MichaelTMiyoshi@users.noreply.github.com>
2026-02-08 14:23:27 -0800

    02/08/2026  Added 2 file reading (and writing) example projects.  FileExample and FileDelimitedExample.  These projects are the same as those given in my book C# with Miyoshi.

---
+++ FIleExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FIleExample;
+
+/*
+    Notice that the output file has an extra newline
+    at the end.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("File Example\n");
+        try
+        {
+            StreamReader reader = new StreamReader("../../../text.txt");
+            StreamWriter writer = new StreamWriter("../../../OutputFile.txt");
+
+            while (!reader.EndOfStream)
+            {
+                String line = reader.ReadLine();
+                Console.WriteLine(line);
+                writer.WriteLine(line);
+            }
+            writer.Close();
+            reader.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ FileDelimitedExample/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FileDelimitedExample;
+
+/*
+    Notice that the output file has an extra newline
+    at the end.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Formatted Data Example!");
+        try
+        {
+            StreamReader reader = new StreamReader("../../../CommaDelimited.txt");
+            StreamWriter writer = new StreamWriter("../../../TabDelimted.txt");
+
+            while (!reader.EndOfStream)
+            {
+                String line = reader.ReadLine();
+                Console.WriteLine(line);
+                String [] data = line.Split(',');
+                String name = data[0];
+                char ch = Convert.ToChar(data[1]);
+                int i = Convert.ToInt32(data[2]);
+                writer.WriteLine($"{name}\t{ch}\t{i}");
+                //writer.WriteLine(name + '\t' + ch + '\t' + i);
+            }
+            writer.Close();
+            reader.Close();
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
